534821a85d30bfb301ae9cb450f5ac8caccfece8
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2019-11-04 11:38:02 -0600

    I added the seedData that was provided to us in my Configuration.cs in Migrations

--- WebAPIStarter-master/WebAPIStarter-master/WebAPISample/Migrations/Configuration.cs
+++ WebAPIStarter-master/WebAPIStarter-master/WebAPISample/Migrations/Configuration.cs
@@ -15,9 +15,17 @@ namespace WebAPISample.Migrations
         protected override void Seed(WebAPISample.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
-
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            context.Movies.AddOrUpdate
+                (
+                    new Models.Movie { Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" },
+                    new Models.Movie { Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" },
+                    new Models.Movie { Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" },
+                    new Models.Movie { Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" },
+                    new Models.Movie { Title = "Die Hard", Genre = "Action", Director = "John McTiernan" }
+                );
+            
         }
     }
 }
